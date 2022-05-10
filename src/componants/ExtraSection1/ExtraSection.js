ed94b8cae985e12446c836c548bb1782636f0045
Sarfaraj116 <sarfarajshaikat116@outlook.com>
2022-05-10 06:14:35 +0600

    modified section two

--- src/componants/ExtraSection1/ExtraSection.js
+++ src/componants/ExtraSection1/ExtraSection.js
@@ -9,10 +9,10 @@ const ExtraSection = () => {
                 <div className="row align-items-center">
                     <div className="col col-12 col-lg-6">
                         <h2>About our warhouse</h2>
-                        <p>The stunning collection of vehicles, which has grown gradually at the site on a business park in Tottenham have been given guide prices of up to £25,000 with the most expensive being a 1960 MG MGA red sports car.</p>
+                        <p>The stunning collection of Laptops, which has grown gradually at the site on a business park in Tottenham have been given guide prices of up to £25,000 with the most expensive being a 1960 MG MGA Laptop house.</p>
                     </div>
                     <div className="col col-12 col-lg-6">
-                        <img style={{ width: '120%' }} src="https://lightscarsaction.com/wp-content/uploads/2017/01/about-car-storage1.jpg" alt="" />
+                        <img style={{ width: '120%' }} src="https://www.thenewsminute.com/sites/default/files/styles/news_detail/public/Amazon_Warehouse_Wikimedia%20Commons%20-%20%C3%81lvaro%20Ib%C3%A1%C3%B1ez.JPG" alt="" />
                     </div>
                 </div>
             </div>
--- src/componants/ExtraSectionTwo/ExtraSectionTwo.js
+++ src/componants/ExtraSectionTwo/ExtraSectionTwo.js
@@ -7,12 +7,12 @@ const ExtraSectionTwo = () => {
                 <div className="row ">
 
                     <div className="col col-12 col-lg-6">
-                        <img style={{ width: '100%' }} src="https://waterlooclassics.com/wp-content/uploads/2017/03/Screen-Shot-2017-02-27-at-16.35.07.png" alt="" />
+                        <img style={{ width: '100%' }} src="https://i.pinimg.com/originals/9d/07/39/9d073908e963af5587bea02d48a6b842.jpg" alt="" />
                     </div>
 
                     <div className="col col-12 col-lg-6">
                         <h2>About our Showroom</h2>
-                        <p>The stunning collection of vehicles, which has grown gradually at the site on a business park in Tottenham have been given guide prices of up to £25,000 with the most expensive being a 1960 MG MGA red sports car.</p>
+                        <p>The stunning collection of Laptop, which has grown gradually at the site on a business park in Tottenham have been given guide prices of up to £25,000 with the most expensive being a 1960 MG MGA Laptop Shop.</p>
                         <p>There will be prizes for several categories, categories to be announced before the show.Tickets and submissions are not open yet. Another email will announce ticket sales.</p>
                     </div>
 
